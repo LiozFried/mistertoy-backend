b25a1c495244bd8d09b708aedca89e4b28eb57e7
Lioz Fired <friedlioz@gmail.com>
2025-09-20 23:37:26 +0300

    Update requireAuth

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -1,29 +1,31 @@
-import { authService } from '../api/auth/auth.service.js'
+import { config } from '../config/index.js'
 import { loggerService } from '../services/logger.service.js'
+import { asyncLocalStorage } from '../services/als.service.js'
 
 export async function requireAuth(req, res, next) {
-    if (!req?.cookies?.loginToken) {
-        return res.status(401).send('Not Authenticated')
-    }
-
-    const loggedinUser = authService.validateToken(req.cookies.loginToken)
-    if (!loggedinUser) return res.status(401).send('Not Authenticated')
+    const { loggedinUser } = asyncLocalStorage.getStore()
     req.loggedinUser = loggedinUser
 
+    if (config.isGuestMode && !loggedinUser) {
+        req.loggedinUser = {
+            _id: '',
+            fullname: 'Guest',
+        }
+        return next()
+    }
+
+    if (!loggedinUser) return res.status(401).send('Not Authenticated')
     next()
 }
 
-export async function requireAdmin(req, res, next) {
-    if (!req?.cookies?.loginToken) {
-        return res.status(401).send('Not Authenticated')
-    }
+export function requireAdmin(req, res, next) {
+    const { loggedinUser } = asyncLocalStorage.getStore()
 
-    const loggedinUser = authService.validateToken(req.cookies.loginToken)
-    if (!loggedinUser?.isAdmin) {
-        loggerService.warn(loggedinUser.fullname + 'attempted to perform admin action')
+    if (!loggedinUser) return res.status(401).send('Not Authenticated')
+    if (!loggedinUser.isAdmin) {
+        logger.warn(loggedinUser.fullname + 'attempted to perform admin action')
         res.status(403).end('Not Authorized')
         return
     }
-
     next()
 }
