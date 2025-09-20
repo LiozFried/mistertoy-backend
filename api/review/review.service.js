703f1c7fa2dc554948a296e9feda46e7d962e705
Lioz Fired <friedlioz@gmail.com>
2025-09-20 22:48:18 +0300

    Create review.service

--- api/review/review.service.js
+++ api/review/review.service.js
@@ -19,9 +19,85 @@ async function query(filterBy = {}) {
 
         const collection = await dbService.getCollection('review')
 
-        var review
+        var review = await collection.aggregate([
+            { $match: criteria },
+            {
+                $lookup: {
+                    from: 'user',
+                    foreignField: '_id',
+                    localField: 'byUserId',
+                    as: 'byUser',
+                    pipeline: [
+                        { $set: { 'userId': '$_id' } },
+                        { $unset: ['_id', 'password'] }
+                    ]
+                }
+            },
+            { $set: { byUser: { $arrayElemAt: ["byUser", 0] } } },
+            {
+                $lookup: {
+                    from: 'toy',
+                    foreignField: '_id',
+                    localField: 'aboutToyId',
+                    as: 'aboutToy',
+                    pipeline: [
+                        { $set: { toyId: '$_id' } },
+                        { $unset: ['_id'] }
+                    ]
+                }
+            },
+            { $unwind: '$aboutToy' },
+            {
+                $project: {
+                    byUserId: 0,
+                    aboutToyId: 0,
+                    'aboutToy.labels': 0,
+                    'aboutToy.createdAt': 0,
+                    msgs: 0
+                }
+            }
+        ]).toArray()
+
+        return review
     } catch (err) {
-        
+        loggerService.error('Cannot get reviews', err)
+        throw err
+    }
+}
+
+async function remove(reviewId) {
+    try {
+        const { loggedinUser } = asyncLocalStorage.getStore()
+        const collection = await dbService.getCollection('review')
+
+        const criteria = { _id: ObjectId.createFromHexString(reviewId) }
+        if (!loggedinUser.isAdmin) {
+            criteria.byUserId = ObjectId.createFromHexString(loggedinUser._id)
+        }
+
+        const { deletedCount } = await collection.deleteOne(criteria)
+        return deletedCount
+    } catch (err) {
+        loggerService.error(`Cannot remove review ${reviewId}`, err)
+        throw err
+    }
+}
+
+async function add(review) {
+    try {
+        const reviewToAdd = {
+            byUserId: ObjectId.createFromHexString(review.byUserId),
+            aboutToyId: ObjectId.createFromHexString(review.aboutToyId),
+            txt: review.txt,
+        }
+
+        const collection = await dbService.getCollection('review')
+        await collection.insertOne(reviewToAdd)
+
+        return reviewToAdd
+    } catch (err) {
+        loggerService.error('Cannot add review', err)
+        throw err
     }
 }
 
